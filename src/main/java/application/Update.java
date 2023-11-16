1edfb9f95fcc1934f9bf7bc8000982aaa8357b53
AbibiJ <146185204+AbibiJ@users.noreply.github.com>
2023-11-16 14:21:04 +0000

    Update and Archive working (20%)
    archive

--- src/main/java/application/Update.java
+++ src/main/java/application/Update.java
@@ -9,16 +9,16 @@ public class Update {
     public static void main(String[] args) {
 
         updateProperty();
-       // archiveProperty();
+        archiveProperty();
     }
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost:8081/ihl_db";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/ihl_db";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
 
     public static void updateProperty() {
         try {
-            Connection connection  = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+            Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
             String query1 = "SELECT * FROM properties WHERE id= ?";
             String query = "UPDATE properties SET street=?, city=?, listingNum=?, styleId=?, typeId=?, bedrooms=?, " +
                     "bathrooms=?, squarefeet=?, berRating=?, description=?, lotsize=?, garagesize=?, garageId=?, " +
@@ -39,30 +39,30 @@ public class Update {
             ps.setString(9, "NEW BERRATING");
             ps.setString(10, "NEW DESCRIPTION");
             ps.setString(11, "NEW LOTSIZE");
-            ps.setInt(12,  1);
+            ps.setInt(12, 1);
             ps.setInt(13, 101);
             ps.setInt(14, 201);
             ps.setString(15, "NEW PHOTO");
             ps.setDouble(16, 35);
-            ps.setDate(17, new java.sql.Date(2023, 11,15));
+            ps.setDate(17, java.sql.Date.valueOf("2023-11-15"));
             ps.setInt(18, 1001);
-
 
 
             int rowsAffected = ps.executeUpdate();
             System.out.println(rowsAffected + " row(s) updated.");
-        }
-        catch (Exception e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+
     public static void archiveProperty() {
         try (Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)) {
-            String query = "UPDATE properties SET archived=true WHERE id=?";
+            String query = "UPDATE properties SET description = CONCAT(description, ' [Archived]') WHERE id = ?";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setString(1, "NEW ID");
+                preparedStatement.setInt(1, 1); // Replace 1 with the actual ID of the property you want to archive
 
                 int rowsAffected = preparedStatement.executeUpdate();
                 System.out.println(rowsAffected + " row(s) archived.");
@@ -72,3 +72,6 @@ public class Update {
         }
     }
 }
+
+
+
--- src/main/java/model/Property.java
+++ src/main/java/model/Property.java
@@ -3,13 +3,7 @@ import lombok.*;
 import org.apache.commons.lang3.builder.ToStringExclude;
 
 import java.util.Date;
-import model.Property;
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 
 
